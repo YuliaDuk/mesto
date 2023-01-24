b71cb22ed5c506241fba39f6a0b39ef6669254f9
YuliaDuk <yule444ka1234@mail.ru>
2023-01-24 22:57:11 +0300

    feat: добавить форму по БЭМ и плавное откытие

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,68 @@
-let popupform = document.querySelector('.popup');
-let addButton = document.querySelector('.profile__button');
-let nameInput = document.querySelector('.popup__item_type_name');
-let jobInput = document.querySelector('.popup__item_type_job');
-let formElement = document.querySelector('.popup__form');
-let closeButton = document.querySelector('.popup__close-icon');
-let changeName = document.querySelector('.profile__name');
-let changeJob = document.querySelector('.profile__description');
+const popupform = document.querySelector('.popup_type_red');
+const addform = document.querySelector('.popup_type_add');
 
-function addForm(){
+const popupButton = document.querySelector('.profile__button');
+const plusButton = document.querySelector('.profile__add-button');
+
+const nameInput = document.querySelector('.popup__item_el_name');
+const jobInput = document.querySelector('.popup__item_el_job');
+const formElement = document.querySelector('.popup__form_type_red');
+const closeButton = document.querySelectorAll('.popup__close-icon');
+const changeName = document.querySelector('.profile__name');
+const changeJob = document.querySelector('.profile__description');
+
+//открытие формы ред
+function popupopenForm(){
     nameInput.value = changeName.textContent;
     jobInput.value = changeJob.textContent;
     popupform.classList.add('popup_opened');
 }
-addButton.addEventListener('click', addForm);
+popupButton.addEventListener('click', popupopenForm);
 
-function closeForm(){
-    popupform.classList.remove('popup_opened');
-}
-closeButton.addEventListener('click', closeForm);
-
+//редактирование формы ред
 function handleFormSubmit (evt) {
     evt.preventDefault();      
     changeName.textContent = nameInput.value;
     changeJob.textContent = jobInput.value;
-    closeForm();
+    popupform.classList.remove('popup_opened');
 }
 formElement.addEventListener('submit', handleFormSubmit); 
 
-const plusButton = document.querySelector('.profile__add-button');
-const escButton = document.querySelector('.popupAddform__close-icon');
-const newPic = document.querySelector('.popupAddform')
-function addPicture(){
-    newPic.classList.add('popupAddform_opened');
+//открытие формы добавление
+function addOpenForm(){
+    addform.classList.add('popup_opened');
 }
-function closePicture(){
-    newPic.classList.remove('popupAddform_opened');
-}
-plusButton.addEventListener('click', addPicture);
-escButton.addEventListener('click', closePicture);
+plusButton.addEventListener('click', addOpenForm);
+
+//закрытие всех форм
+closeButton.forEach(item => {
+    item.addEventListener('click', function(evt){
+        evt.target.closest('.popup').classList.remove('popup_opened');
+    });
+});
+
+
+
+// function addPicture(){
+//     newPic.classList.add('popupAddform_opened');
+// }
+// function closePicture(){
+//     newPic.classList.remove('popupAddform_opened');
+// }
+// plusButton.addEventListener('click', addPicture);
+// escButton.addEventListener('click', closePicture);
+
+// const deleteButton = document.querySelectorAll('.element__trash');
+// deleteButton.forEach(item => {
+//     item.addEventListener('click', () => {
+//         const deleteElement = item.closest('.element');
+//         deleteElement.remove();
+//     })
+// });
+// const Elements = document.querySelector('.elements');
+
+// Elements.addEventListener('click', function(evt){
+//     const deleteButtonElement = evt.target;
+//     const deleteElement = deleteButtonElement.closest('.element');
+//     deleteElement.remove();
+// });
