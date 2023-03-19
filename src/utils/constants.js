9ed71ba2cafea6916f43be9fa5fe0463af1a6f94
YuliaDuk <yule444ka1234@mail.ru>
2023-03-19 17:57:24 +0300

    feat: настроить вебпак

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,28 +1,17 @@
+const arkhys = new URL('../images/arkhyz.jpg', import.meta.url);
+const chelyabinskRegion = new URL('../images/chelyabinsk-oblast.jpg', import.meta.url);
+const ivanov = new URL('../images/ivanovo.jpg', import.meta.url);
+const kamchatka = new URL('../images/kamchatka.jpg', import.meta.url);
+const holmogorsk = new URL('../images/kholmogorsky-rayon.jpg', import.meta.url);
+const baikal = new URL('../images/baikal.jpg', import.meta.url);
+
 export const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
+    { name: 'Архыз', link: arkhys},
+    { name: 'Челябинская область', link: chelyabinskRegion },
+    { name: 'Иваново', link: ivanov },
+    { name: 'Камчатка', link: kamchatka },
+    { name: 'Холмогорский район', link: holmogorsk },
+    { name: 'Байкал', link: baikal }
   ]; 
 export const options = {formSelector: '.popup__form',
   inputSelector: '.popup__item',
